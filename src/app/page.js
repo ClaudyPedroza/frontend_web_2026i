232f5f543f9d6afef52243fab65bca8c6bd2cdd7
ClaudyP <pedrozaromeroclaudy@gmail.com>
2026-04-22 19:44:26 -0500

    Corrección del proyecto, organizandólo en carpetas según sus funciones

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,18 @@
 "use client";
 
 import { RadioButton } from "@/components";
+import NasaApod from "@/components/NasaApod";
 import Image from "next/image";
 import { useState } from "react";
-
-const URL = "https://api.nasa.gov/planetary/apod";
-const API_KEY = "TFc5A92bfsTe54hj6QpiONFmcHTyULo7gO1bD0Bn";
-
+import { useNasa } from "@/hooks/useNasa";
 
 export default function Home() {
-
-  // Para manejar los radiobutton
+  // Para manejar los radiobutton, interfaz del usuario por eso estas declaraciones quedan en el page
   const [modo, setModo] = useState("");
   const [fecha, setFecha] = useState("");
   const [count, setCount] = useState("");
 
-  // Manejar los estados de la API recibidos, cargando y error
-  const [datosNasa, setDatosNasa] = useState(null); //variable para manejar cuando lleguen los datos OK👍🏻
-  const [cargando, setCargando] = useState(false);
-  const [error, setError] = useState(null);
+  const { datosNasa, cargando, error, consultarNASA, limpiarResultado } = useNasa();
 
   // null - respuesta de la API y "" - respuesta del usuarip
   const manejoModo = (e) => {
@@ -28,43 +22,15 @@ export default function Home() {
     setModo(valor);
     setFecha(hoyFormatoNasa);
     setCount(1);
-    setDatosNasa(null);
-    setCargando(false);
-    setError(null);
-  }
 
-  const consultarNasa = () => {
-    if (!modo) return //si no está seleccionado no hace nada
-    setCargando(true)
-    let url_final = `${URL}?api_key=${API_KEY}` // Expresión de js 
-    if (modo === "especifica" && fecha) {
-      url_final += `&date=${fecha}`
-    } else if (modo === "aleatoria") {
-      url_final += `&count=${count}`
+    if (valor === "hoy") {
+      setFecha(hoyFormatoNasa);
+    } else if (valor === "especifica") {
+      setFecha("");
+    } else if (valor === "aleatoria") {
+      setCount(1);
     }
-    //Consumir API mediante el fetch
-    fetch(url_final).then((response) => {
-      if (!response.ok) throw new Error("Error al consultar el API de la NASA")
-      return response.json();
-    }).then((datos) => {
-      console.log("Los datos estan aqui", datos);
-      if (Array.isArray(datos)) {
-        setDatosNasa(datos);
-      } else {
-        setDatosNasa([datos]);
-      } setCargando(false);
-    }).catch((err) => {
-      setError(err.message)
-      setCargando(false);
-    })
-  }
-
-  const limpiarResultado = () => {
-    setModo("");
-    setFecha("");
-    setDatosNasa(null);
-    setCargando(false);
-    setError(null);
+    limpiarResultado();
   }
 
   return (
@@ -133,7 +99,7 @@ export default function Home() {
           </div>
         )}
 
-        <button className="rounded-2xl bg-blue-800 hover:bg-blue-400 transition-colors text-white font-medium my-3.5 mx-3.5 px-6 py-3" id="buscar" onClick={consultarNasa} disabled={cargando}>
+        <button className="rounded-2xl bg-blue-800 hover:bg-blue-400 transition-colors text-white font-medium my-3.5 mx-3.5 px-6 py-3" id="buscar" onClick={() => consultarNASA(modo, fecha, count)} disabled={cargando}>
           Consultar NASA APOD
         </button>
 
@@ -146,24 +112,7 @@ export default function Home() {
         {error && <p className="text-red-500 text-center my-5">{error}</p>}
 
         {datosNasa && datosNasa.map((item, index) => (
-          <div key={index} className="border-gray-300 rounded-3xl border-2 text-xl mx-8 my-5 py-8 px-8 bg-white/10 backdrop-blur-md text-white dark:bg-zinc-900">
-            <h2 className="font-bold text-3xl mb-2">{item.title}</h2>
-            <p className="text-white mb-4">{item.date}</p>
-
-            <div className="flex justify-center mb-6">
-              {item.media_type === "image" ? (
-                <img src={item.url} alt={item.title} className="rounded-xl max-w-full h-auto shadow-lg" />
-              ) : (
-                <a href={item.url} target="_blank" className="text-blue-600 underline">
-                  Abrir video en una nueva pestaña
-                </a>
-              )}
-            </div>
-
-            <p className="text-lg leading-relaxed text-justify">
-              {item.explanation}
-            </p>
-          </div>
+          <NasaApod key={index} item={item} />
         ))}
       </div>
     </div>
